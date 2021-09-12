ac8c800c8fdf7106e22bbfa870ed0e61bdb1c625
Kevin Ngo <kevinngo.cal@gmail.com>
2021-09-11 22:17:46 -0400

    Update styling of dapp using Chakra UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,11 @@
-import React from "react";
-import Lottie from "react-lottie";
-import { Web3ApiProvider } from "@web3api/react";
-
-import { HelloWorld } from "./HelloWorld";
-import Web3ApiAnimation from "./lottie/Web3API_Icon_Cycle.json";
-import "./App.css";
+import React from 'react';
+import { Web3ApiProvider } from '@web3api/react';
+import { HelloWorld } from './HelloWorld';
 
 export const App: React.FC = () => {
-
-  const logoLottieOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: Web3ApiAnimation,
-  };
-
   return (
-    <div className="main">
-      <Web3ApiProvider>
-        <Lottie
-          options={logoLottieOptions}
-          isClickToPauseDisabled={true}
-          height={"300px"}
-          width={"300px"}
-          style={{
-            width: "50%",
-            height: "auto",
-            maxWidth: "300px"
-          }}
-        />
-        <HelloWorld />
-      </Web3ApiProvider>
-    </div>
+    <Web3ApiProvider>
+      <HelloWorld />
+    </Web3ApiProvider>
   );
 };
--- src/HelloWorld.tsx
+++ src/HelloWorld.tsx
@@ -1,15 +1,65 @@
-import React, { useEffect } from "react";
-import { useWeb3ApiQuery } from "@web3api/react";
-import { CHAIN_ID, COVALENT_API, COVALENT_API_KEY } from "./config";
-import { CovalentAccountBalance, CovalentResponse, CovalentTokenBalance } from "./interfaces";
-import { Token } from "./components/Token";
+import React from 'react';
+import { useWeb3ApiQuery } from '@web3api/react';
+import { CHAIN_ID, COVALENT_API, COVALENT_API_KEY } from './config';
+import {
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Heading,
+  Button,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+} from '@chakra-ui/react';
+import { Nav } from './Nav';
+
+interface TokenBalance {
+  contract_decimals: number;
+  contract_name: string;
+  contract_ticker_symbol: string;
+  contract_address: string;
+  supports_erc: string[];
+  logo_url: string;
+  type: string;
+  balance: number;
+  balance_24h: number;
+  quote_rate: number;
+  quote_rate_24h: number;
+  quote: number;
+  quote_24h: number;
+  nft_data: unknown | null;
+}
+
+interface AccountBalance {
+  address: string;
+  updated_at: string;
+  next_update_at: string;
+  quote_currency: string | null;
+  chain_id: number;
+  pagination: unknown | null;
+  items: TokenBalance[];
+}
+
+interface CovalentResponse {
+  data: AccountBalance;
+  error: boolean | null;
+  error_message: string | null;
+  error_code: string | null;
+}
 
 export const HelloWorld: React.FC = () => {
-  const [message, setMessage] = React.useState("");
-  const [accountBalance, setAccountBalance] = React.useState<CovalentAccountBalance | undefined>(undefined);
+  const [message, setMessage] = React.useState('');
+  const [accountBalance, setAccountBalance] = React.useState<
+    AccountBalance | undefined
+  >(undefined);
 
-  const covalentQuery = {
-    uri: "w3://ens/http.web3api.eth",
+  const query = {
+    uri: 'w3://ens/http.web3api.eth',
     query: `
       query {
         get(
@@ -23,15 +73,19 @@ export const HelloWorld: React.FC = () => {
     `,
   };
 
-  const { execute: executeCovalent } = useWeb3ApiQuery(covalentQuery);
+  const { execute } = useWeb3ApiQuery(query);
 
   const logMsgHandler = async (): Promise<any> => {
-    console.info(`Sending Query: ${JSON.stringify(covalentQuery, null, 2)}`);
-    const result = await executeCovalent();
-    if(result && result.data && result.data?.get) {
-      const response: Record<string, string> = result.data.get as Record<string, string>;
-      const covalentResponse = JSON.parse(response["body"]) as CovalentResponse;
+    console.info(`Sending Query: ${JSON.stringify(query, null, 2)}`);
+    const result = await execute();
+    if (result && result.data && result.data?.get) {
+      const response: Record<string, string> = result.data.get as Record<
+        string,
+        string
+      >;
+      const covalentResponse = JSON.parse(response['body']) as CovalentResponse;
       setAccountBalance(covalentResponse.data);
+      console.log(covalentResponse.data);
     }
   };
 
@@ -41,50 +95,38 @@ export const HelloWorld: React.FC = () => {
 
   return (
     <>
-      <div className="main__heading">Portfolio tracker</div>
-      <div className="main__text">
-        Enter your ethereum address:<br/>
-      </div>
-      <input
-        className="main__input"
-        onChange={(event) => onChangeHandler(event)}
-      />
-      <button className="main__btn" onClick={logMsgHandler}>
-        Submit
-      </button>
-      <br/>
-      <div className="main__table">
-        {
-          accountBalance && accountBalance.items.map( (token: CovalentTokenBalance, i) => (
-            <Token key={i} index={i} token={token}/>
-          ))
-        }
-      </div>
-      <div style={{ width: "100%", height: "100px" }}/>
-      <div className="footer__container">
-        <a
-          className="footer__link"
-          href="https://github.com/Web3-API/demos/tree/main/hello-world/web3api"
-          target="_blank"
-        >
-          Source Code
-        </a>
-        <a
-          className="footer__link"
-          href="https://app.ens.domains/name/helloworld.web3api.eth"
-          target="_blank"
-        >
-          ENS Domain
-        </a>
-        <a
-          className="footer__link"
-          href="https://bafybeig7r7vm6vg7fkv4u57p6pj3t3a7li56zeiiu6nn7sx5lrlacy7lpi.ipfs.dweb.link/"
-          target="_blank"
-        >
-          IPFS Package
-        </a>
-      </div>
+      <Nav />
+      <Box p={10}>
+        <Heading mb={15}>Portfolio Tracker</Heading>
+        <FormControl id='email'>
+          <FormLabel>Wallet address</FormLabel>
+          <Flex maxW={'33rem'}>
+            <Input mb={5} mr={5} onChange={(event) => onChangeHandler(event)} />
+            <Button onClick={logMsgHandler} colorScheme='teal'>
+              Submit
+            </Button>
+          </Flex>
+        </FormControl>
+        <br />
+
+        <Table maxW='80%' variant='striped' colorScheme='teal'>
+          <Thead>
+            <Tr>
+              <Th>Token</Th>
+              <Th>Balance</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {accountBalance &&
+              accountBalance.items.map((token: TokenBalance, i) => (
+                <Tr>
+                  <Td> {token.contract_ticker_symbol} </Td>
+                  <Td> {token.balance / 10 ** token.contract_decimals} </Td>
+                </Tr>
+              ))}
+          </Tbody>
+        </Table>
+      </Box>
     </>
   );
-}
-
+};
