175143ac2df9681acba00486faa5042048aff1c4
Niraj Kamdar <nirajkamdar27@pm.me>
2021-09-18 18:24:24 +0530

    fix: use typescript instead of js

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
 import { ChakraProvider, extendTheme, ColorModeScript } from '@chakra-ui/react';
-import colors from './theme.js';
+import colors from './theme';
 
 const theme = extendTheme({ colors });
 
